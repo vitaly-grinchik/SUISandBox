cb775597b808866905596351c2ed1e8e8b62e59f
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2024-02-07 00:35:58 +0300

    Import Foundation

--- SUISandBox/Models/DataStore.swift
+++ SUISandBox/Models/DataStore.swift
@@ -1,4 +1,6 @@
 /// Data for app
+import Foundation
+
 final class DataStore {
     
     static let shared = DataStore()    
@@ -320,6 +322,28 @@ final class DataStore {
              list.append(randomUser)
          }
          return list
+    }
+    
+    func load<T: Decodable>(_ filename: String) -> T {
+        let data: Data
+
+        guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+        }
+
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+        }
     }
     
     private init() {}
